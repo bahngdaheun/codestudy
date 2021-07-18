9e679aad6fa778b37ddde7f2214d7a9146c48d63
bahngdaheun <reparo@naver.com>
2021-07-18 16:11:50 +0900

    .

---
+++ app/src/main/java/javastudy/Variable.java
@@ -0,0 +1,13 @@
+package javastudy;
+
+public class Variable {
+
+  public static void main(String[] args) {
+    float f = 1.23456f;
+    System.out.println(f);
+    //System.out.printf("f = %014.10", f);
+    System.out.printf("f = %014.10f\n", f);
+    System.out.printf("f = %14.10f", f);
+  }
+
+}
