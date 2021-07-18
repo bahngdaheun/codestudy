b028f618bb6da3e470139a36d3c94367c3df3229
bahngdaheun <reparo@naver.com>
2021-07-18 23:36:29 +0900

    .

--- app/src/main/java/javastudy/Variable.java
+++ app/src/main/java/javastudy/Variable.java
@@ -7,7 +7,15 @@ public class Variable {
     System.out.println(f);
     //System.out.printf("f = %014.10", f);
     System.out.printf("f = %014.10f\n", f);
-    System.out.printf("f = %14.10f", f);
+    System.out.printf("f = %14.10f\n", f);
+
+    double d = 1.0e100;
+    float f2 = (float)d;
+    System.out.println(f2);
+
+    double d2 = 1.0e-50;
+    float f3 = (float)d2;
+    System.out.println(f3);
   }
 
 }
