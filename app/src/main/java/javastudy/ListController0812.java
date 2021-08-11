8c62812e108d727d0eefbac21a45aa1ef2fb7256
bahngdaheun <reparo@naver.com>
2021-08-12 02:56:55 +0900

    msg_web

---
+++ app/src/main/java/javastudy/ListController0812.java
@@ -0,0 +1,18 @@
+package kr.go.practice1.controller;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+@Log4j2
+@WebServlet(name = "ListController", value = "/msg/list")
+public class ListController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        log.info("list controller");
+        request.getRequestDispatcher("/WEB-INF/msg/list.jsp").forward(request,response);
+    }
+}
