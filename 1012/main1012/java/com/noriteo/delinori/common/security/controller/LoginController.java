f9f6cbb36027501ea8630f1c47078542558f2983
bahngdaheun <reparo@naver.com>
2021-10-12 11:08:57 +0900

    project1012

---
+++ 1012/main1012/java/com/noriteo/delinori/common/security/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.noriteo.delinori.common.security.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class LoginController {
+
+    @GetMapping("/customLogin")
+    public void loginCustom(){
+        log.warn("custom login...get");
+    }
+
+}
